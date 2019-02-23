f37110727c097d4cce2177285175667f552b536a
ayyappa1 <b.ayyappa@gmail.com>
2019-02-23 08:44:08 -0600

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "web" {
-  name        = "web-access"
+  name        = "web-access1"
   description = "Security group to allow access from mgmt vpc"
   vpc_id      = "vpc-093b7371"
    tags {
